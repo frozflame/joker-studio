2e7d775af61c525d555aae55dc142071b413124d
frozflame <frozflame@outlook.com>
2019-06-26 10:50:30 +0800

    avatar: crop center, ANTIALIAS, default 320x320

--- joker/studio/misc/avatar.py
+++ joker/studio/misc/avatar.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import os
+import pathlib
+import sys
 
 from PIL import Image
 
@@ -11,15 +12,23 @@ class AvatarMaker(object):
         self.size = size
         self.ext = ext
 
-    def make_filename(self, name):
-        base, _ = os.path.splitext(name)
-        dimension = '.{0}x{0}'.format(self.size)
-        return base + dimension + self.ext
-
     def convert_image(self, path):
         im = Image.open(path).convert("RGB")
-        im = im.resize((self.size, self.size))
-        im.save(self.make_filename(path))
+        w, h = im.size
+        el = min(im.size)
+        box = [
+            int((w - el) / 2.),
+            int((h - el) / 2.),
+            int((w + el) / 2.),
+            int((h + el) / 2.),
+        ]
+        im = im.crop(box).resize((self.size, self.size), Image.ANTIALIAS)
+        px = pathlib.Path(path)
+        prefix = 'avatar-{0}x{0}.'.format(self.size)
+        name = prefix + px.stem + self.ext
+        outpath = str(px.with_name(name))
+        print(outpath, file=sys.stderr)
+        im.save(outpath)
 
     @classmethod
     def batch_convert(cls, size, ext, paths):
@@ -36,7 +45,7 @@ def run(prog=None, args=None):
         '-f', '--format', choices=['jpg', 'png'], default='jpg',
         help='output image format')
     parser.add_argument(
-        '-s', '--size', type=int, default=160,
+        '-s', '--size', type=int, default=320,
         help='size of output image')
     parser.add_argument(
         'filenames', metavar='FILENAME', nargs='+',
@@ -44,4 +53,3 @@ def run(prog=None, args=None):
     ns = parser.parse_args(args)
     ext = '.' + ns.format
     AvatarMaker.batch_convert(ns.size, ext, ns.filenames)
-

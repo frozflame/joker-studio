de111b37c8627a30e66d52ca027bc17fc33233f0
frozflame <frozflame@outlook.com>
2021-07-03 15:31:17 +0800

    use cmddef -- __main__.py

--- joker/studio/__main__.py
+++ joker/studio/__main__.py
@@ -3,25 +3,25 @@
 
 from volkanic.system import CommandRegistry
 
-entries = {
-    'joker.studio.ffmpeg.conv': 'conv',
-    'joker.studio.ffmpeg.crop': 'crop',
-    'joker.studio.ffmpeg.fade': 'fade',
-    'joker.studio.ffmpeg.split': 'split',
-    'joker.studio.ffmpeg.subtitle': 'sub',
-    'joker.studio.ffmpeg.thumb:thumb': 'thumb',
-    'joker.studio.ffmpeg.thumb:poster': 'poster',
-    'joker.studio.misc.avatar': 'avatar',
-    'joker.studio.misc.avatar:mkico': 'ico',
-    'joker.studio.misc.avatar:mkimb': 'imb',
-    'joker.studio.misc.avatar:report_wh_ratios': 'iwh',
-    'joker.studio.misc.margin': 'imt',
-    'joker.studio.misc.rename': 'ren',
-    'joker.studio.misc.vident': 'vid',
-}
+cmddef = """\
+conv        joker.studio.ffmpeg.conv
+crop        joker.studio.ffmpeg.crop
+fade        joker.studio.ffmpeg.fade
+split       joker.studio.ffmpeg.split
+sub         joker.studio.ffmpeg.subtitle
+thumb       joker.studio.ffmpeg.thumb:thumb
+poster      joker.studio.ffmpeg.thumb:poster
+avatar      joker.studio.misc.avatar
+ico         joker.studio.misc.avatar:mkico
+imb         joker.studio.misc.avatar:mkimb
+iwh         joker.studio.misc.avatar:report_wh_ratios
+imt         joker.studio.misc.margin
+ren         joker.studio.misc.rename
+vid         joker.studio.misc.vident
+"""
 
-prog = 'python3 -m joker.sudio'
-registry = CommandRegistry.from_entries(entries, prog)
+_prog = 'python3 -m joker.sudio'
+registry = CommandRegistry.from_cmddef(cmddef, _prog)
 
 if __name__ == '__main__':
     registry()
